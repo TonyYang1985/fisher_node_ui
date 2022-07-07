f6c5d69d85a2987bdbe67853add0469f50294f5c
Tony <yangxiangjiang19880805@gmail.com>
2022-07-07 20:07:25 +0800

    change

--- src/libs/components/MenuActive.tsx
+++ src/libs/components/MenuActive.tsx
@@ -2,25 +2,81 @@ import _ from 'lodash';
 import Router, { useRouter } from 'next/router';
 import { useCallback, useLayoutEffect, useState } from 'react';
 
-export const useMenuActive = () => {
+export const useActiveMenu = () => {
   const { locale } = useRouter();
-  const [menuActive, setMenuActive] = useState<number>(0);
+  const [activeMenu, setActiveMenu] = useState<number>(0);
 
+  /******Active Menu *****/
   useLayoutEffect(() => {
-    const menuActive = localStorage.getItem('sg.fot.menuActive') ?? '0';
-    const activeIndex = parseInt(menuActive);
-    setMenuActive(activeIndex);
+    const menuActive = localStorage.getItem('sg.fot.ActiveMenu') ?? '0';
+    const activeMenu = parseInt(menuActive);
+    setActiveMenu(activeMenu);
   }, []);
 
-  const changeMenuActive = useCallback((activeIndex: number) => {
+  const changeActiveMenu = useCallback((activeIndex: number) => {
     if (_.isNumber(activeIndex)) {
-      localStorage.setItem('sg.fot.menuActive', `${activeIndex}`);
-      setMenuActive(activeIndex);
+      localStorage.setItem('sg.fot.ActiveMenu', `${activeIndex}`);
+      setActiveMenu(activeIndex);
     } else {
-      localStorage.removeItem('sg.fot.menuActive');
+      localStorage.removeItem('sg.fot.ActiveMenu');
     }
     Router.replace(Router.asPath, Router.asPath, { locale: locale });
   }, []);
 
-  return { menuActive, changeMenuActive };
+  return { activeMenu, changeActiveMenu };
+};
+
+export const useActiveRootMenu = () => {
+  const { locale } = useRouter();
+  const [activeRootIndex, setActiveRootIndex] = useState<number>(0);
+
+  useLayoutEffect(() => {
+    const activeRoot = localStorage.getItem('sg.fot.ActiveRoot') ?? '0';
+    const activeRootIndex = parseInt(activeRoot);
+    setActiveRootIndex(activeRootIndex);
+  }, []);
+
+  const changeActiveRootMenu = useCallback((activeIndex: number) => {
+    if (_.isNumber(activeIndex)) {
+      const activeRoot = localStorage.getItem('sg.fot.ActiveRoot') ?? '0';
+      const activeRootIndex = parseInt(activeRoot);
+      if (activeRootIndex !== activeIndex) {
+        localStorage.setItem('sg.fot.ActiveRoot', `${activeIndex}`);
+        setActiveRootIndex(activeIndex);
+      }
+    } else {
+      localStorage.removeItem('sg.fot.ActiveRoot');
+    }
+    Router.replace(Router.asPath, Router.asPath, { locale: locale });
+  }, []);
+
+  return { activeRootIndex, changeActiveRootMenu };
+};
+
+export const useExtendRootMenu = () => {
+  const [extendRootIndex, setExtendRootIndex] = useState<number>(0);
+
+  useLayoutEffect(() => {
+    const activeRoot = localStorage.getItem('sg.fot.ExtendRoot') ?? '0';
+    const activeRootIndex = parseInt(activeRoot);
+    setExtendRootIndex(activeRootIndex);
+  }, []);
+
+  const changeExtendRootMenu = useCallback((activeIndex: number) => {
+    const extendRoot = localStorage.getItem('sg.fot.ExtendRoot') ?? '0';
+    const extendRootIndex = parseInt(extendRoot);
+    if (_.isNumber(activeIndex)) {
+      if (extendRootIndex === activeIndex) {
+        localStorage.setItem('sg.fot.ExtendRoot', `0`);
+        setExtendRootIndex(0);
+      } else {
+        localStorage.setItem('sg.fot.ExtendRoot', `${activeIndex}`);
+        setExtendRootIndex(activeIndex);
+      }
+    } else {
+      localStorage.removeItem('sg.fot.ExtendRoot');
+    }
+  }, []);
+
+  return { extendRootIndex, changeExtendRootMenu };
 };
