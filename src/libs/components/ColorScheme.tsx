8a2b36f40bc767eaa183bb8735466cb340a9eb36
Tony <yangxiangjiang19880805@gmail.com>
2022-08-03 15:16:52 +0800

    change

--- src/libs/components/ColorScheme.tsx
+++ src/libs/components/ColorScheme.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import Router, { useRouter } from 'next/router';
+import { useCallback, useLayoutEffect, useState } from 'react';
 
 export const ColorScheme = () => {
-  useEffect(() => {
+  useLayoutEffect(() => {
     const colorScheme = localStorage.getItem('sg.fot.ColorScheme') ?? 'dark';
     if (colorScheme) {
       localStorage.setItem('sg.fot.ColorScheme', `${colorScheme}`);
@@ -12,6 +13,7 @@ export const ColorScheme = () => {
 
 export const useColorScheme = () => {
   const [colorScheme, setColorScheme] = useState<string>('dark');
+  const { locale } = useRouter();
 
   useLayoutEffect(() => {
     const colorScheme = localStorage.getItem('sg.fot.ColorScheme') ?? 'dark';
@@ -22,7 +24,10 @@ export const useColorScheme = () => {
     if (color) {
       localStorage.setItem('sg.fot.ColorScheme', `${color}`);
       setColorScheme(color);
+    } else {
+      localStorage.removeItem('sg.fot.ColorScheme');
     }
+    Router.replace(Router.asPath, Router.asPath, { locale: locale });
   }, []);
 
   return { colorScheme, changeColorScheme };
